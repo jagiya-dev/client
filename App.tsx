2546a5ec6bbae3d2f792d28eb6fb9828c8ab0150
yoonsang lee <highp0912@pm.me>
2023-07-31 21:52:29 +0900

    add: tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import {StatusBar, Text, View} from 'react-native';
 
 const App = () => {
-  return <div>App!</div>;
+  return (
+    <View className="flex-1 items-center justify-center">
+      <StatusBar barStyle="default" />
+      <Text className="px-5 text-gray-400 text-2xl font-bold mt-2">
+        Open up App.tsx to start working on your app!
+      </Text>
+    </View>
+  );
 };
 
 export default App;
