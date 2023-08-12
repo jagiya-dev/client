81ae75b599a0042777b8b69d1e16dce823271ed6
yoonsang lee <highp0912@pm.me>
2023-08-12 18:14:09 +0900

    add: codepush android setup

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+
+// https://learn.microsoft.com/en-us/appcenter/distribution/codepush/rn-plugin
 import codePush from 'react-native-code-push';
 
 const App = () => {
