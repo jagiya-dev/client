3e5a295d2e1186c222eb7c5808ed7f11a4e7b24a
yoonsang lee <highp0912@pm.me>
2023-08-16 00:19:39 +0900

    add: react-nativation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import type { FC } from "react";
-import { SafeAreaView, StyleSheet, StatusBar, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Codepush from "@/util/codepush";
 import MainPage from "@/screens/Main";
+import React from "react";
+
+const Stack = createNativeStackNavigator();
 
 const App: FC = () => {
   // run codepush first of all
@@ -12,18 +17,15 @@ const App: FC = () => {
   }
 
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <MainPage />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <StatusBar />
+        <Stack.Navigator initialRouteName="Main">
+          <Stack.Screen name="Main" component={MainPage} />
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
 export default Codepush.hoc(App);
-
-const style = StyleSheet.create({
-  root: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
