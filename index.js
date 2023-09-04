5c36e60ea54d24d0bf0895250f305fc957b64bf2
yoonsang lee <highp0912@pm.me>
2023-09-05 02:17:03 +0900

    fix: codepush duplicated link err

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
+import {firebase} from "@react-native-firebase/messaging";
+
+firebase.initializeApp({
+
+});
 
 AppRegistry.registerComponent(appName, () => App);
