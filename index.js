58bcc2159d3a0069325682517c9484a253ae7bb8
yoonsang lee <highp0912@pm.me>
2023-09-05 07:09:32 +0900

    add: firebase performance monitoring

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ import App from './App';
 import {name as appName} from './app.json';
 import {firebase} from "@react-native-firebase/messaging";
 
-// if (Platform.OS === 'android') {
-//     firebase.initializeApp({
-//         projectId: "",
-//         appId: "1:726991405564:android:1c4cdcabd725cd28bc7279",
-//         apiKey: "AIzaSyDiBovdm6V_ntkiQ5iYcYvETAAqOLuUhko",
-//         databaseURL: "",
-//         messagingSenderId: "",
-//         storageBucket: undefined,
-//         persistence: true,
-//     }, "readyUmbrella");
-//
-// } else if (Platform.OS === 'ios') {
-//     firebase.initializeApp({
-//         projectId: "",
-//         appId: "1:726991405564:ios:af43900f3a4c49f2bc7279",
-//         apiKey: "AIzaSyApSyzwVlHuhVCXGzzaOgWX49nMVcI8IAY",
-//         databaseURL: "",
-//         messagingSenderId: "",
-//         storageBucket: undefined,
-//         persistence: true,
-//     }, "readyUmbrella");
-// }
-//
+if (Platform.OS === 'android') {
+    firebase.initializeApp({
+        projectId: "",
+        appId: "1:726991405564:android:1c4cdcabd725cd28bc7279",
+        apiKey: "AIzaSyDiBovdm6V_ntkiQ5iYcYvETAAqOLuUhko",
+        databaseURL: "",
+        messagingSenderId: "",
+        storageBucket: undefined,
+        persistence: true,
+    }, "readyUmbrella");
+
+} else if (Platform.OS === 'ios') {
+    firebase.initializeApp({
+        projectId: "",
+        appId: "1:726991405564:ios:af43900f3a4c49f2bc7279",
+        apiKey: "AIzaSyApSyzwVlHuhVCXGzzaOgWX49nMVcI8IAY",
+        databaseURL: "",
+        messagingSenderId: "",
+        storageBucket: undefined,
+        persistence: true,
+    }, "readyUmbrella");
+}
+
 
 AppRegistry.registerComponent(appName, () => App);
