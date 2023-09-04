8edbc5970cd45b9298fb36f9540c53357b5247bf
yoonsang lee <highp0912@pm.me>
2023-09-05 07:06:41 +0900

    add: firebase analytics / crashlytics / remote-config

--- index.js
+++ index.js
@@ -2,13 +2,33 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {firebase} from "@react-native-firebase/messaging";
 
-firebase.initializeApp({
-
-});
+// if (Platform.OS === 'android') {
+//     firebase.initializeApp({
+//         projectId: "",
+//         appId: "1:726991405564:android:1c4cdcabd725cd28bc7279",
+//         apiKey: "AIzaSyDiBovdm6V_ntkiQ5iYcYvETAAqOLuUhko",
+//         databaseURL: "",
+//         messagingSenderId: "",
+//         storageBucket: undefined,
+//         persistence: true,
+//     }, "readyUmbrella");
+//
+// } else if (Platform.OS === 'ios') {
+//     firebase.initializeApp({
+//         projectId: "",
+//         appId: "1:726991405564:ios:af43900f3a4c49f2bc7279",
+//         apiKey: "AIzaSyApSyzwVlHuhVCXGzzaOgWX49nMVcI8IAY",
+//         databaseURL: "",
+//         messagingSenderId: "",
+//         storageBucket: undefined,
+//         persistence: true,
+//     }, "readyUmbrella");
+// }
+//
 
 AppRegistry.registerComponent(appName, () => App);
