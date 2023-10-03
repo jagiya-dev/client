d546f6906c972b91d6813583beeb622de4a8851f
yoonsang lee <highp0912@pm.me>
2023-10-03 20:51:23 +0900

    add: link audios platform specifically to native

--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,10 @@
 // react native config goes as follows
 module.exports = {
     assets: ['./assets/fonts/'],
+    iosAssets: [
+        './assets/audio/ios'
+    ],
+    androidAssets: [
+        './assets/audio/android'
+    ]
 };
