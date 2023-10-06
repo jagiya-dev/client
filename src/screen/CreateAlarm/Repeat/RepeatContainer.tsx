5d2ec73b5a3dd16b83003a9d84bca9b96eceb855
yoonsang lee <highp0912@pm.me>
2023-10-06 20:35:48 +0900

    add: 백엔드 API codegen

--- src/screen/CreateAlarm/Repeat/RepeatContainer.tsx
+++ src/screen/CreateAlarm/Repeat/RepeatContainer.tsx
@@ -1,7 +1,17 @@
 import Text from "@/components/Text";
+import { getAlarmDetail } from "@/network/api";
+import { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 
 const RepeatContainer = () => {
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await getAlarmDetail({ alarmId: "1" });
+      console.log(res);
+    };
+    fetch();
+  }, []);
+
   return (
     <View>
       <Text>반복</Text>
