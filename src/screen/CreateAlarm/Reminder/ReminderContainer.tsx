bdfb0ab8c91282a7f1b5f836ad955a68389de003
yoonsang lee <highp0912@pm.me>
2023-10-13 00:33:20 +0900

    refac: data 정리

--- src/screen/CreateAlarm/Reminder/ReminderContainer.tsx
+++ src/screen/CreateAlarm/Reminder/ReminderContainer.tsx
@@ -6,72 +6,10 @@ import { widthPercentageToDP } from "react-native-responsive-screen";
 import { ReminderIntervalItem } from "@/typing";
 import { BottomSheetScrollView, useBottomSheet } from "@gorhom/bottom-sheet";
 import BottomButton from "@/components/fixed/BottomButton";
-
-const reminderItemsIOS: readonly ReminderIntervalItem[] = [
-  ...new Array(4).fill(0).map((_, i) => ({ id: i.toString(), label: "" })),
-  {
-    id: "4",
-    label: "1분",
-  },
-  {
-    id: "5",
-    label: "3분",
-  },
-  {
-    id: "6",
-    label: "5분",
-  },
-  {
-    id: "7",
-    label: "10분",
-  },
-  {
-    id: "8",
-    label: "15분",
-  },
-  {
-    id: "9",
-    label: "30분",
-  },
-  {
-    id: "10",
-    label: "60분",
-  }, // 6
-  ...new Array(6).fill(0).map((_, i) => ({ id: i.toString(), label: "" })),
-];
-
-const reminderItemsAndroid: readonly ReminderIntervalItem[] = [
-  ...new Array(4).fill(0).map((_, i) => ({ id: i.toString(), label: "" })),
-  {
-    id: "4",
-    label: "1분",
-  },
-  {
-    id: "5",
-    label: "3분",
-  },
-  {
-    id: "6",
-    label: "5분",
-  },
-  {
-    id: "7",
-    label: "10분",
-  },
-  {
-    id: "8",
-    label: "15분",
-  },
-  {
-    id: "9",
-    label: "30분",
-  },
-  {
-    id: "10",
-    label: "60분",
-  }, // 6
-  ...new Array(8).fill(0).map((_, i) => ({ id: i.toString(), label: "" })),
-];
+import {
+  reminderItemsAndroid,
+  reminderItemsIOS,
+} from "@/screen/CreateAlarm/Reminder/Reminder.data";
 
 const ReminderContainer = () => {
   const { close } = useBottomSheet();
@@ -82,7 +20,6 @@ const ReminderContainer = () => {
 
   const reminderItems =
     Platform.OS === "ios" ? reminderItemsIOS : reminderItemsAndroid;
-  console.log(reminderItems);
 
   return (
     <View style={s.root}>
